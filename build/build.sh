86b77297013cb8aeeef5bbb55b7afaf0556b5da0
Pierre-Gilles Leymarie <pierregilles.leymarie@gmail.com>
2017-05-08 10:14:41 +0200

    build script

---
+++ build/build.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+  then
+    echo "You must pass a specific tag."
+    echo "Ex: ./build.sh v3.0.0"
+    exit 1
+fi
+
+arch=$(arch)
+os=$(uname -s)
+dest_file=gladys-$1-$os-$arch.tar.gz
+
+echo "Building file $dest_file"
+
+# clean folder if exit
+rm -rf gladys
+
+# Cloning repository
+git clone https://github.com/GladysProject/Gladys gladys
+
+# Go to folder
+cd gladys
+
+# Checkout to tags
+git checkout tags/$1
+
+# remove git folder
+rm -rf .git
+
+# install dependencies
+yarn install
+
+# build assets
+grunt buildProd
+
+# Move back
+cd ..
+
+# creating archive
+tar zcvf $dest_file ./gladys
